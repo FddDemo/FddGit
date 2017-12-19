0e9282933287653dde3249fb770820ee30dcae2d
hedomym <31910498+hedomym@users.noreply.github.com>
2017-12-19 11:55:44 +0800

    sql

---
+++ SQL.java
@@ -0,0 +1,307 @@
+package com.ctw.common.enums;
+
+/**
+ * SQL关键字
+ *	
+ * @author LongJun
+ *
+ *        2017年11月3日 上午9:51:05
+ */
+public interface SQL {
+
+	String SELECT = "SELECT";
+	
+	String UPDATE = "UPDATE";
+	
+	String DELETE = "DELETE";
+	
+	String INSERT = "INSERT";
+	
+	String FROM = "FROM";
+	
+	String ABSOLUTE = "ABSOLUTE";
+	
+	String AND = "AND";
+	
+	String ADD = "ADD";
+	
+	String ADDDATE = "ADDDATE";
+	
+	String AFTER = "AFTER";
+	
+	String ALTER = "ALTER";
+	
+	String ALL = "ALL";
+	
+	String ASC = "ASC";
+	
+	String AS = "AS";
+	
+	String ANY = "ANY";
+	
+	String BEFORE = "BEFORE";
+	
+	String BEGIN = "BEGIN";
+	
+	String BETWEEN = "BETWEEN";
+	
+	String BOTH = "BOTH";
+	
+	String BY = "BY";
+	
+	String BIT = "BIT";
+	
+	String BOOLEAN = "BOOLEAN";
+	
+	String CHAR = "CHAR";
+	
+	String CHARACTER = "CHARACTER";
+	
+	String CHECK = "CHECK";
+	
+	String CLASS = "CLASS";
+	
+	String COMMIT = "COMMIT";
+	
+	String CONNECT= "CONNECT";
+	
+	String CONSTRAINT = "CONSTRAINT";
+	
+	String CONTINUE = "CONTINUE";
+	
+	String COUNT = "COUNT";
+	
+	String CREATE = "CREATE";
+	
+	String CREATEDB = "CREATEDB";
+	
+	String CREATEUSER = "CREATEUSER";
+	
+	String CROSS = "CROSS";
+	
+	String CURRENT = "CURRENT";
+	
+	String CURRENT_PATH = "CURRENT_PATH";
+	
+	String CURRENT_ROLE = "CURRENT_ROLE";
+	
+	/**
+	 * 格式：2017-11-03
+	 */
+	String CURRENT_DATE = "CURRENT_DATE";
+	
+	/**
+	 * 格式：10:37:34
+	 */
+	String CURRENT_TIME = "CURRENT_TIME";
+	
+	/**
+	 * 格式：2017-11-03 10:37:49
+	 */
+	String CURRENT_TIMESTAMP = "CURRENT_TIMESTAMP"; 
+	
+	String CURRENT_USER = "CURRENT_USER";
+	
+	String DAY = "DAY";
+	
+	String DATE = "DATE";
+	
+	String DEC = "DEC";
+	
+	String DECIMAL = "DECIMAL";
+	
+	String DECLARE = "DECLARE";
+	
+	String DEFAULT = "DEFAULT";
+	
+	String DESC = "DESC";
+	
+	String DESTROY = "DESTROY";
+	
+	String DISTINCT = "DISTINCT";
+	
+	String DO = "DO";
+	
+	String DOUBLE = "DOUBLE";
+	
+	String DROP = "DROP";
+	
+	String EACH = "EACH";
+	
+	String ELSE = "ELSE";
+	
+	String END = "END";
+	
+	String EQUALS = "EQUALS";
+	
+	String EVERY = "EVERY";
+	
+	String EXCEPT = "EXCEPT";
+	
+	String EXEC = "EXEC";
+	
+	String EXECUTE = "EXECUTE";
+	
+	String EXISTS = "EXISTS";
+	
+	String FOR = "FOR";
+	
+	String FUNCTION = "FUNCTION";
+	
+	String FALSE = "FALSE";
+	
+	String FLOAT = "FLOAT";
+	
+	String GLOBAL = "GLOBAL";
+	
+	String GOTO = "GOTO";
+	
+	String GROUP = "GROUP";
+	
+	String HAVING = "HAVING";
+	
+	String HOUR = "HOUR";
+	
+	String IDENTITY = "IDENTITY";
+	
+	String IN = "IN";
+	
+	String INCREMENT = "INCREMENT";
+	
+	String INT = "INT";
+	
+	String INTEGER = "INTEGER";
+	
+	String INTO = "INTO";
+	
+	String IS = "IS";
+	
+	String JOIN = "JOIN";
+	
+	String LAST = "LAST";
+	
+	String LEFT = "LEFT";
+	
+	String LENGTH = "LENGTH";
+	
+	String LESS = "LESS";
+	
+	String LIKE = "LIKE";
+	
+	String LIMIT = "LIMIT";
+	
+	String LOWER = "LOWER";
+	
+	String MAX = "MAX";
+	
+	String MIN = "MIN";
+	
+	String MINUTE = "MINUTE";
+	
+	String MODE = "MODE";
+
+	String NOT = "NOT";
+	
+	String NULL = "NULL";
+	
+	String NUMBER = "NUMBER";
+	
+	String NUMERIC = "NUMERIC";
+	
+	String OF = "OF";
+	
+	String ON = "ON";
+	
+	String ONLY = "ONLY";
+	
+	String OR = "OR";
+	
+	String ORDER = "ORDER";
+	
+	String PRIMARY = "PRIMARY";
+	
+	String RETURN = "RETURN";
+	
+	String RIGHT = "RIGHT";
+	
+	String ROLLBACK = "ROLLBACK";
+	
+	String SELF = "SELF";
+	
+	String SEQUENCE = "SEQUENCE";
+	
+	String SET = "SET";
+	
+	String SUM = "SUM";
+	
+	String TRUE = "TRUE";
+	
+	String TABLE = "TABLE";
+	
+	String THEN = "THEN";
+	
+	String THAN = "THAN";
+	
+	String TIME = "TIME";
+	
+	String TIMESTAMP = "TIMESTAMP";
+	
+	String TIMEZONE_HOUR = "TIMEZONE_HOUR";
+	
+	String TIMEZONE_MINUTE = "TIMEZONE_MINUTE";
+	
+	String TO = "TO";
+	
+	String TREAT = "TREAT";
+	
+	String TRIM = "TRIM";
+	
+	String UNION = "UNION";
+	
+	String UNIQUE = "UNIQUE";
+	
+	String UPPER = "UPPER";
+	
+	String USING = "USING";
+	
+	String VARCHAR = "VARCHAR";
+	
+	String VERSION = "VERSION";
+	
+	String WHEN = "WHEN";
+	
+	String WHERE = "WHERE";
+	
+	String WITH = "WITH";
+	
+	String WITHOUT = "WITHOUT";
+	
+	String WHENEVER = "WHENEVER";
+	
+	String YEAR = "YEAR";
+	
+	String ZONE = "ZONE";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
